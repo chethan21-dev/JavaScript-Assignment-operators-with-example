653112f865df78c504199f2963d76299f7846fb1
chethan21-dev <chethanchowdary21@gmail.com>
2024-09-24 23:11:37 +0530

    Add files via upload

---
+++ JavaScript Assignment operators/JavaScript Assignment Operators.js
@@ -0,0 +1,86 @@
+JS assignment operator - 
+The assignment ( = ) operator is used to assign a value to a variable or property
+
+1. = assignment operator - 
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript = Operator</h1>
+<p id="demo"></p>
+<script>
+let x = 10;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+2. += assignment operator - 
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript  += operator</h1>
+<p id="demo"></p>
+<script>
+var x = 10;
+x += 5;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+3. -= assignment operator - 
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript -= Operator</h1>
+<p id="demo"></p>
+<script>
+var x = 10;
+x -= 5;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+4. *= assignment operator - 
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>JavaScript *= operator</h1>
+<p id="demo"></p>
+<script>
+var x = 10;
+x *= 5;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+5. /= assignment operator - 
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript /= operator</h1>
+<p id="demo"></p>
+<script>
+var x = 10;
+x /= 5;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
+
+6. %= assignment operator - 
+<!DOCTYPE html>
+<html>
+<body>
+<h1>JavaScript %= operator</h1>
+<p id="demo"></p>
+<script>
+var x = 10;
+x %= 5;
+document.getElementById("demo").innerHTML = x;
+</script>
+</body>
+</html>
